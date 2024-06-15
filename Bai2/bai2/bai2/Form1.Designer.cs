b0772111a7ae83391ce02fbb5d986a404d735e4d
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-06-15 10:22:42 +0700

    Add files via upload

---
+++ Bai2/bai2/bai2/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btdecrypt = new System.Windows.Forms.Button();
+            this.btencrypt = new System.Windows.Forms.Button();
+            this.txtdecrypt = new System.Windows.Forms.TextBox();
+            this.txtencrypt = new System.Windows.Forms.TextBox();
+            this.txtinput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtkey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btdecrypt);
+            this.groupBox1.Controls.Add(this.btencrypt);
+            this.groupBox1.Controls.Add(this.txtdecrypt);
+            this.groupBox1.Controls.Add(this.txtencrypt);
+            this.groupBox1.Controls.Add(this.txtinput);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtkey);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 3);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(607, 467);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mã hóa Vigenère";
+            // 
+            // btdecrypt
+            // 
+            this.btdecrypt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btdecrypt.Location = new System.Drawing.Point(315, 15);
+            this.btdecrypt.Name = "btdecrypt";
+            this.btdecrypt.Size = new System.Drawing.Size(143, 33);
+            this.btdecrypt.TabIndex = 9;
+            this.btdecrypt.Text = "Giải mã";
+            this.btdecrypt.UseVisualStyleBackColor = true;
+            this.btdecrypt.Click += new System.EventHandler(this.btdecrypt_Click);
+            // 
+            // btencrypt
+            // 
+            this.btencrypt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btencrypt.Location = new System.Drawing.Point(166, 15);
+            this.btencrypt.Name = "btencrypt";
+            this.btencrypt.Size = new System.Drawing.Size(143, 33);
+            this.btencrypt.TabIndex = 8;
+            this.btencrypt.Text = "Mã hóa";
+            this.btencrypt.UseVisualStyleBackColor = true;
+            this.btencrypt.Click += new System.EventHandler(this.btencrypt_Click);
+            // 
+            // txtdecrypt
+            // 
+            this.txtdecrypt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdecrypt.Location = new System.Drawing.Point(7, 370);
+            this.txtdecrypt.Multiline = true;
+            this.txtdecrypt.Name = "txtdecrypt";
+            this.txtdecrypt.Size = new System.Drawing.Size(560, 91);
+            this.txtdecrypt.TabIndex = 7;
+            // 
+            // txtencrypt
+            // 
+            this.txtencrypt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtencrypt.Location = new System.Drawing.Point(7, 231);
+            this.txtencrypt.Multiline = true;
+            this.txtencrypt.Name = "txtencrypt";
+            this.txtencrypt.Size = new System.Drawing.Size(560, 104);
+            this.txtencrypt.TabIndex = 6;
+            // 
+            // txtinput
+            // 
+            this.txtinput.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtinput.Location = new System.Drawing.Point(7, 104);
+            this.txtinput.Multiline = true;
+            this.txtinput.Name = "txtinput";
+            this.txtinput.Size = new System.Drawing.Size(560, 104);
+            this.txtinput.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Giải mã";
+            // 
+            // txtkey
+            // 
+            this.txtkey.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkey.Location = new System.Drawing.Point(51, 51);
+            this.txtkey.Name = "txtkey";
+            this.txtkey.Size = new System.Drawing.Size(516, 22);
+            this.txtkey.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã hóa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 472);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btdecrypt;
+        private System.Windows.Forms.Button btencrypt;
+        private System.Windows.Forms.TextBox txtdecrypt;
+        private System.Windows.Forms.TextBox txtencrypt;
+        private System.Windows.Forms.TextBox txtinput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtkey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Bai2/bai2/bai2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string VigenereEncrypt(string text, string key)
+        {
+            StringBuilder result = new StringBuilder();
+            key = key.ToUpper();
+            int keyIndex = 0;
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    char k = char.IsUpper(c) ? key[keyIndex] : char.ToLower(key[keyIndex]);
+                    result.Append((char)(((c + k - 2 * offset) % 26) + offset));
+                    keyIndex = (keyIndex + 1) % key.Length;
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
+
+        private string VigenereDecrypt(string text, string key)
+        {
+            StringBuilder result = new StringBuilder();
+            key = key.ToUpper();
+            int keyIndex = 0;
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    char k = char.IsUpper(c) ? key[keyIndex] : char.ToLower(key[keyIndex]);
+                    result.Append((char)(((c - k + 26) % 26) + offset));
+                    keyIndex = (keyIndex + 1) % key.Length;
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
+
+        private void btencrypt_Click(object sender, EventArgs e)
+        {
+            string input = txtinput.Text;
+            string key = txtkey.Text;
+            txtencrypt.Text = VigenereEncrypt(input, key);
+        }
+
+        private void btdecrypt_Click(object sender, EventArgs e)
+        {
+            string input = txtencrypt.Text;
+            string key = txtkey.Text;
+            txtdecrypt.Text = VigenereDecrypt(input, key);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtdecrypt.Clear();
+            txtencrypt.Clear();
+            txtinput.Clear();
+            txtkey.Clear();
+        }
+    }
+}
