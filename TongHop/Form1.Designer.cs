cd96cee3334387bb3afffb40520f53bdecc373be
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-06-15 10:24:00 +0700

    Add files via upload

---
+++ TongHop/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TongHop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btcaeser = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btcaeser
+            // 
+            this.btcaeser.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btcaeser.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcaeser.ForeColor = System.Drawing.Color.Maroon;
+            this.btcaeser.Location = new System.Drawing.Point(261, 9);
+            this.btcaeser.Name = "btcaeser";
+            this.btcaeser.Size = new System.Drawing.Size(88, 71);
+            this.btcaeser.TabIndex = 0;
+            this.btcaeser.Text = "Caeser";
+            this.btcaeser.UseVisualStyleBackColor = false;
+            this.btcaeser.Click += new System.EventHandler(this.btcaeser_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Peru;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Info;
+            this.button2.Location = new System.Drawing.Point(166, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Vigenère.";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(-1, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackgroundImage = global::TongHop.Properties.Resources._1700102633867;
+            this.splitContainer1.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.splitContainer1.Panel1.Controls.Add(this.btcaeser);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackgroundImage = global::TongHop.Properties.Resources.images;
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Size = new System.Drawing.Size(715, 294);
+            this.splitContainer1.SplitterDistance = 367;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 299);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btcaeser;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ TongHop/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TongHop
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private string CaesarEncrypt(string text, int shift)
+        {
+            return new string(text.Select(c => EncryptChar(c, shift)).ToArray());
+        }
+
+        private string CaesarDecrypt(string text, int shift)
+        {
+            return CaesarEncrypt(text, 26 - shift);
+        }
+
+        private char EncryptChar(char c, int shift)
+        {
+            if (!char.IsLetter(c))
+                return c;
+
+            char offset = char.IsUpper(c) ? 'A' : 'a';
+            return (char)(((c + shift - offset) % 26) + offset);
+        }
+
+        private void btencrypt_Click(object sender, EventArgs e)
+        {
+            string input = txtinput.Text;
+            int shift = int.Parse(txtshift.Text);
+            txtencrypt.Text = CaesarEncrypt(input, shift);
+        }
+
+        private void btdecrypt_Click(object sender, EventArgs e)
+        {
+            string input = txtencrypt.Text;
+            int shift = int.Parse(txtshift.Text);
+            txtdecrypt.Text = CaesarDecrypt(input, shift);
+        }
+
+        private void btclear_Click(object sender, EventArgs e)
+        {
+            txtdecrypt.Clear();
+            txtencrypt.Clear();
+            txtinput.Clear();
+            txtshift.Clear();
+        }
+    }
+}
